d8c83041e2924aab56785dc0468c3103c8af4827
Andrii <andreiverkienko1095@gmail.com>
2022-08-23 18:21:15 +0200

    changes

--- random.js
+++ random.js
@@ -1,40 +1,52 @@
 
+const wolunteers= [
+	"Maks:",
+	"Wika:",
+	"Yana:",
+	"Wania:",
+	"Denis:",
+	"Kasper:",
+	"Marta:",
+	"Yehor:",
+	"Karolina:",
+	"Wlados:"
+];
+const place = [
+	"lidl",
+	"biedra",
+	"kauf",
+	"marshe",
+	"eklerk",
+	"aldi",
+	"Lewiatan",
+	"Carefour",
+	"Zabka",
+	"Alkohole24"
+];
 
-function createList() {
-    const i = Math.floor(Math.random() * 10);
-    const j = Math.floor(Math.random() * 10);
-    
-    
-    const wolunteers = [
-        'Maks',
-        'Wika',
-        'Yana',
-        'Wania',
-        'Denis',
-        'Kasper',
-        'Marta',
-        'Yehor',
-        'Karolina',
-        'Wlados'
-    ];
-    const place = [
-        'lidl',
-        'biedra',
-        'kauf',
-        'marshe',
-        'eklerk',
-        'aldi',
-        'Lewiatan',
-        'Carefour',
-        'Zabka',
-        'Alkohole24'
-    ];
-    
-    console.log(`${wolunteers[i]}: ${place[j]}`);
-    
+// const wolunteersRandom = place[Math.random() * place.length];
+// // console.log(wolunteersRandom);
 
-}
+// const createArr = ([...place], maxLength) => Array.from(
+// 	{ length: Math.min(place.length, 1 + Math.random() * maxLength | 0) },
+// 	() => place.splice(Math.random() * place.length | 0, 1)[0]
+//  );
 
-for (let i = 0; i < 10; i++) {
-    createList();
-}
+//  console.log(createArr);
+
+// let randomPlace = place.sort(()=>Math.random());
+
+// // let wyjazd = `${wolunteers}+${randomPlace}`;
+// // console.log(wyjazd);
+
+
+// let newArrRandom = wolunteers.concat(randomPlace);
+
+// console.log(newArrRandom);
+
+
+const photos = [{img: 'a', type: 'b'}, {img: 'c', type: 'd'}];
+
+const links = photos.map(({ img, type }) => ({ image: img, imgType: type }));
+
+console.log(links);
